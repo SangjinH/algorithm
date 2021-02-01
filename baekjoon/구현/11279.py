1f4fc0628f5e19d770748b4d8c02cb5b3ddd7211
SangjinH <54434944+SangjinH@users.noreply.github.com>
2021-02-01 22:02:31 +0900

    최대 max heapq 구현

---
+++ baekjoon/구현/11279.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/11279
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+
+q = []
+
+for _ in range(n):
+    num = int(input())
+
+    if not num:
+        if len(q):
+            print(-heapq.heappop(q))
+        else:
+            print(0)
+
+    else:
+        heapq.heappush(q, -num)
+        # print(q)
