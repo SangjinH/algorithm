3738cb1fa8ad49400eba3c794075cbd90ebdd303
SangjinH <54434944+SangjinH@users.noreply.github.com>
2021-02-17 13:33:15 +0900

    bfs 기본

--- baekjoon/DFS_BFS/2667_2.py
+++ baekjoon/DFS_BFS/2667_2.py
@@ -5,12 +5,38 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
+
 def bfs(arr, x, y):
     q = deque([(x, y)])
     arr[x][y] = 0
-    
+    cnt = 1
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if (0 <= nx < n) and (0 <= ny < n) and arr[nx][ny] == 1:
+                cnt += 1
+                arr[nx][ny] = 0
+                q.append((nx, ny))
+
+    return cnt
+
 
 n = int(input())
 arr = []
-for _ in range(n):
-    arr.append(list(input().split()))
+for i in range(n):
+    arr.append(list(map(int,input().rstrip())))
+apt = 0
+counts = []
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            apt += 1
+            counts.append(bfs(arr, i, j))
+counts.sort()
+print(apt)
+for i in range(len(counts)):
+    print(counts[i])
